b93c2874cb272d8b8fd7586d6c35a3a7057f9db4
yuri.izi <yuri.izi@hotmail.com>
2024-06-16 17:34:12 -0400

    criando rotas e conexão com mongodb

---
+++ public/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Chat />} />
+    </Routes>
+  </BrowserRouter>;
+}
