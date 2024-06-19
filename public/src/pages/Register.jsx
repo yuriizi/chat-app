51b6dac767b944389fa962958998fc6cdf4a4104
yuri.izi <yuri.izi@hotmail.com>
2024-06-18 23:58:20 -0400

    Formulario de Registro com rota para backend

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,58 +1,119 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoutes";
 function Register() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert("form");
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { password, username, email } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem("chat-app", JSON.stringify(data.user));
+        navigate("/");
+      }
     }
-    const handleChange = (event) => {
+  };
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
 
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
     }
 
-    return (
-        <>
-            <FormContainer>
-                <form onSubmit={(event) => handleSubmit(event)}>
-                    <div className='brand'>
-                        <img src={Logo} alt='Logo' />
-                        <h1>snappy</h1>
-                    </div>
-                    <input
-                        type='text'
-                        placeholder='Username'
-                        name='username'
-                        onChange={e => handleChange(e)}
-                    />
-                    <input
-                        type='email'
-                        placeholder='Email'
-                        name='email'
-                        onChange={e => handleChange(e)}
-                    />
-                    <input
-                        type='password'
-                        placeholder='Password'
-                        name='password'
-                        onChange={e => handleChange(e)}
-                    />
-                    <input
-                        type='password'
-                        placeholder='Confirm Password'
-                        name='confirmPassword'
-                        onChange={e => handleChange(e)}
-                    />
-                    <button type='submit'>Create User</button>
-                    <span>
-                        Already have an account ? <Link to="/login">Login</Link >
-                    </span>
-                </form>
-            </FormContainer>;
-        </>
+    return true;
+  };
 
-    )
+  return (
+    <>
+      <FormContainer>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login.</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
+    </>
+  );
 }
 
 const FormContainer = styled.div`
--- server/index.js
+++ server/index.js
@@ -2,19 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URL, {
+app.use("/api/auth", userRoutes);
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     console.log("DB Connection Successful");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err.message);
-});
+  });
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server Starter on Port ${process.env.PORT}`);
-})
+  console.log(`Server Starter on Port ${process.env.PORT}`);
+});
